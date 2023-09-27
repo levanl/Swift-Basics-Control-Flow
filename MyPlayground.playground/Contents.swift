14f6cca622b971e0712049e586f9ce906fcbaf24
levanloladze <levan.loladzew@gmail.com>
2023-09-27 12:29:34 +0400

    done 1,2,3 assignments

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,42 @@
+
+// 1
+
+
+let ricxvi: Int = 30
+
+let ricxvi2: Int = 15
+
+print(ricxvi + ricxvi2)
+
+
+// 2
+
+
+let ricxvi3: Int = 15
+
+if ricxvi3 > 10 && ricxvi3 <= 20 {
+
+    print("\(ricxvi3) is > 10 and <= 20")
+    
+}else if ricxvi3 < 20 && ricxvi3 <= 50 {
+    
+    print("\(ricxvi3) is > 20 and <= 50")
+    
+}else if ricxvi3 > 50 && ricxvi3 <= 100 {
+
+    print("\(ricxvi3) is > 50 and <= 100")
+    
+}else if ricxvi3 > 100 {
+    
+    print("\(ricxvi3) is > 100")
+    
+} else {
+    
+    print("\(ricxvi3) is < 10")
+    
+}
+
+
+// 3
+
+
